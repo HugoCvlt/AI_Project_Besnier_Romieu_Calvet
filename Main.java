3843c0456750dd62f82daffd5f9519d919730b6d
Matthias Besnier <besnier.matthias@gmail.com>
2022-12-11 22:51:32 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,33 +2,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board(4);
+        Board board = new Board(2);
         
         board.print_board();
         //System.out.println(board.end_test());
         //System.out.println(" h1 :" + Board.h1(board));
         //System.out.println(" h2 :" + Board.h2(board));
 
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        while(!board.end_test()){
+        while (!board.end_test()){
 
             System.out.println("(u, d, l, r) ? ");
-            String play = sc.nextLine();
+            String play = input.nextLine();
             
             switch(play){
-
                 case "u":
                     board.moveUp();
+                    break;
 
                 case "d":
                     board.moveDown();
+                    break;
 
                 case "l":
                     board.moveLeft();
+                    break;
 
                 case "r":
                     board.moveRight();
+                    break;
 
             }//End switch
 
@@ -36,7 +39,7 @@ public class Main {
 
         }//End game
 
-        sc.close();
+        input.close();
     }
 
 }
