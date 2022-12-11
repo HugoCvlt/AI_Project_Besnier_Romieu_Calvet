25fc9ab235d447a6f4b2cafc5be7a5c9e680ca41
Matthias Besnier <besnier.matthias@gmail.com>
2022-12-11 12:45:16 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        Board board = new Board(4);
+        
+        board.print_board();
+        System.out.println(board.end_test());
+        System.out.println(" h1 :" + Board.h1(board));
+        System.out.println(" h2 :" + Board.h2(board));
+    }
 }
