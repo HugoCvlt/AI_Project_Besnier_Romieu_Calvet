31d3b79a4ec7f041acee4e7bf2002e933c4af466
HugoCvlt <hugocalvet66@icloud.com>
2022-12-12 19:39:01 +0100

    Update Board.java

--- Board.java
+++ Board.java
@@ -10,8 +10,9 @@ public class Board{
 
     public Board(int n){
         this.size = n;
-        this.board = random_setBoard(this.size);
+        this.board = new int[n][n];
     }
+
     public Board(Board b) {
 		this.board = new int[b.size][b.size];
         this.size = b.size;
@@ -22,26 +23,47 @@ public class Board{
         }
 	}
 
+    public Board(int n, int[][] mat) throws Exception{
+        if(n != mat.length){ throw new Exception();}
+        this.size = n;
+        this.board = mat;
+    }
+
     public Object clone(){
 		return new Board(this);
 	}
 
-    public static int[][] random_setBoard(int n){
-        ArrayList<Integer> mylist = new ArrayList<Integer>();
-        int [][] board = new int[n][n];
-
-        for (int i=0; i< Math.pow(n,2); i++){
-            mylist.add(i);
-        }
-        Collections.shuffle(mylist);
-        int cpt = 0;
-        for(int i=0; i< n; i++){
-            for(int j=0; j< n; j++){
-                board[i][j] = mylist.get(cpt);
-                cpt++;   
+    public Board random_setBoard(int n, int k){
+        //Create final board
+        int[][] final_mat = new int[n][n];
+        int cpt = 1;
+        for(int i = 0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if((i==n-1) && (j==n-1)){
+                    final_mat[i][j] = 0;
+                }
+                else{
+                    final_mat[i][j] = cpt;
+                }
+                cpt++;
             }
         }
-        return board;
+
+        Board shuffled = new Board(n, final_mat);
+
+        //SHUFLLE
+        String available_action;
+        int indiceRand;
+        char move;
+        for(int l = 0; l<k; l++){
+            available_action = shuffled.get_action_available();
+            indiceRand = Math.random()*(available_action.length-1);
+            move = available_action.charAt(indiceRand);
+
+            shuffled.move(move);
+
+        }
+        return shuffled;
     }
 
     public void print_board(){
@@ -54,22 +76,27 @@ public class Board{
     }
 
     public Boolean end_test(){
-        int cpt = 1;
+        int cpt =1;
         for(int i=0; i< this.size;i++){
             for(int j=0; j< this.size; j++){
-                if ((i != this.size - 1) && (j != this.size - 1)){
-                    if (this.board[i][j] != cpt){
+
+                if ((i == this.size - 1) && (j == this.size - 1)){
+                    if (this.board[i][j] != 0){
                        return false;      
                     }
-                }else{
-                    if (this.board[i][j] != 0){
+                }
+
+                else{
+                    if (this.board[i][j] != cpt){
                         return false;
                     }
                 }
+
+                cpt++;
             }
-            cpt++;
         }
         return true;
+
     }
 
     public static int h1(Board b){
@@ -189,6 +216,27 @@ public class Board{
             this.board[zero_row][zero_column + 1] = 0;
         }
 
+    }
+
+    public void move(char x){
+        switch(x){
+            case 'u':
+                this.moveUp();
+                break;
+
+            case 'd':
+                this.moveDown();
+                break;
+
+            case 'l':
+                this.moveLeft();
+                break;
+
+            case 'r':
+                this.moveRight();
+                break;
+
+        }//End switch
     }
 
 }
