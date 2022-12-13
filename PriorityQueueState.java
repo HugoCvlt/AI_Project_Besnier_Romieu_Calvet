b3b5b9a28c48d0524ea1ec44de08e977c6a67ae8
Matthias Besnier <besnier.matthias@gmail.com>
2022-12-13 13:22:07 +0100

    Create PriorityQueueState.java
    prioriry queue for uniform seach cost (A* without any heuristic)

---
+++ PriorityQueueState.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class PriorityQueueState{
+	public TreeSet<SearchNode> queue;
+	HashMap<String,SearchNode> set;
+	
+	public PriorityQueueState() {
+		this.queue = new TreeSet<SearchNode>();
+		this.set = new HashMap<String,SearchNode>();
+	}
+	
+	public void push(SearchNode node) {
+		String stateString = node.toString();
+		SearchNode stateFrontier = this.set.get(stateString);
+		if(stateFrontier == null) {
+			this.queue.add(node);
+			this.set.put(stateString,node);
+		}
+		else {
+            // this is where the priority is checked
+			if(stateFrontier.nbrActions > node.nbrActions) {
+				this.queue.remove(stateFrontier);
+				this.set.remove(stateFrontier.toString());
+				this.queue.add(node);
+				this.set.put(stateString,node);
+			}
+		}
+	}
+	
+	public SearchNode pop() {
+		SearchNode current = this.queue.first();
+		this.queue.remove(current);
+		this.set.remove(current.toString());
+		return current;
+	}
+	
+	public int size() {
+		return this.set.size();
+	}
+}
