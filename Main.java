8f520d00e997321684e790ca33e1fa0b70922b25
Matthias Besnier <besnier.matthias@gmail.com>
2022-12-13 11:27:34 +0100

    Update Main.java
    change few things to display the board

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Board board = new Board(3,5);
+        Board board = new Board(4,20);
         
-        board.print_board();
+        board.show_board();
         //System.out.println(board.end_test());
         //System.out.println(" h1 :" + Board.h1(board));
         //System.out.println(" h2 :" + Board.h2(board));
@@ -13,12 +13,16 @@ public class Main {
 
         while (!board.end_test()){
             
-            System.out.println("(" + board.get_action_available() +  ") ?");
+            System.out.print("Available actions : | ");
+            board.get_action_available().chars().forEach(i -> System.out.print((char)i +" | "));
+            System.out.println();
+            System.out.print("=> ");
             char play = input.nextLine().charAt(0);
+            System.out.println();
 
             board.move(play);
 
-            board.print_board();
+            board.show_board();
             System.out.println(board.end_test());
 
         }//End game
