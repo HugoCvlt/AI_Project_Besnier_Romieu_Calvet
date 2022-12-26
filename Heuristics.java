20e37448cf3ef1d9dfad8d9998b81a188ba0e378
Matthias Besnier <besnier.matthias@gmail.com>
2022-12-26 22:11:12 +0100

    Update Heuristics.java
    l'heuristique de la distance de manhattan ne donnait pas la bonne valeur maintenant c'est bon

--- Heuristics.java
+++ Heuristics.java
@@ -1,12 +1,11 @@
-public class Heuristics {
-
+public class Heuristics { 
     
     public static int h1(Board b){
         int cpt = 1;
         int h = 0;
         for (int i=0; i< b.size; i++){
             for (int j=0; j< b.size; j++){
-                if ((i != b.size - 1) && (j != b.size - 1)){
+                if ((i != b.size - 1) || (j != b.size - 1)){
                     if (b.board[i][j] != cpt){
                        h++;      
                     }
@@ -27,7 +26,7 @@ public class Heuristics {
         for (int i=0; i< b.size; i++){
             for (int j=0; j< b.size; j++){
                 if (b.board[i][j] != 0){
-                    h = h + (Math.abs( ((b.board[i][j] - 1 ) % b.size) - j ) + Math.abs((b.board[i][j] / b.size) - i));
+                    h = h + (Math.abs( ((b.board[i][j] - 1 ) % b.size) - j ) + Math.abs(row_nb(b.board[i][j],b) - i));
                 }else{
                     h = h + (Math.abs(b.size - 1 - i) + Math.abs(b.size - 1 - j));
                 }
@@ -35,4 +34,22 @@ public class Heuristics {
         }
     return h;
     }
+
+    public static int row_nb(int value,Board b){
+        int cpt = 1;
+        for(int i=0; i < b.size;i++){
+            for(int j=0 ; j< b.size; j++){
+                
+                if (value == 0){
+                    return b.size - 1;
+                }
+                if (cpt == value){
+                    return i;
+                }
+                cpt++;
+            }
+        }
+        return -1;
+    }
+
 }
