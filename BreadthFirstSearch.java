78e942f191eb568fff92f865a146167f75c6f394
truite_krakenisée <theophile.romieu@gmail.com>
2023-01-05 16:31:56 +0100

    Modification of BFS to use PriorityQueueState

--- BreadthFirstSearch.java
+++ BreadthFirstSearch.java
@@ -6,15 +6,15 @@ import java.util.Collections;
 // compmlexity of BFS O(|S| + |E|) = O(2|S|) = 0(|S|) it's also O(b^d)
 public class BreadthFirstSearch {
     public SearchNode root;
-    public ArrayList<SearchNode> frontier;
-    public ArrayList<SearchNode> explored;
+    public PriorityQueueState frontier;
+    public StateSet explored;
     public int max_size_frontier;
     public int max_size_explore_set;
 
     public BreadthFirstSearch(Board root){
         this.root = new SearchNode(root,0,null,'\0',0);
-        this.frontier = new ArrayList<SearchNode>(); // used as a FIFO queue
-        this.explored = new ArrayList<SearchNode>(); // set of searchNodes
+        this.frontier = new PriorityQueueState(); // used as a FIFO queue
+        this.explored = new StateSet(); // set of searchNodes
         this.max_size_frontier = 0;
         this.max_size_explore_set = 0;
     }
@@ -25,18 +25,18 @@ public class BreadthFirstSearch {
             return new ArrayList<Character>();
         }
 
-        this.frontier.add(curr);
+        this.frontier.push(curr);
         boolean finished = false;
         ArrayList<SearchNode> next = null;
         while(!finished){
-            if(this.frontier.isEmpty()){
+            if(this.frontier.size() == 0){
                 return null;
             }
             if (this.max_size_frontier < this.frontier.size()){
                 this.max_size_frontier = this.frontier.size();
             }
 
-            curr = this.frontier.remove(0);
+            curr = this.frontier.pop();
             this.explored.add(curr);
             next = curr.expand();
             if(next.isEmpty()){
@@ -44,7 +44,7 @@ public class BreadthFirstSearch {
             }
             else{
                 for (SearchNode child : next) {
-                    if(!(this.frontier.contains(child) || this.explored.contains(child))){
+                    if(!(this.frontier.queue.contains(child) || this.explored.contains(child))){
                         if(child.state.end_test()){
                             max_size_explore_set = this.explored.size();
                             //System.out.println("number of expanded state node to find the best solution using BFS : " + this.explored.size()); 
@@ -58,7 +58,7 @@ public class BreadthFirstSearch {
                             return res;
                         }
                         else{
-                            this.frontier.add(child);
+                            this.frontier.push(child);
                         }
                     }
                 }
